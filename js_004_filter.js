7d72878156d5b85a87bf1eb4c3780d15a56be55b
nathaly796 <143623710+nathaly796@users.noreply.github.com>
2024-10-22 11:09:11 -0300

    Add files via upload

---
+++ js_004_filter.js
@@ -0,0 +1,7 @@
+const produtos = [
+    { nome: "notebook", preco: 6000.00, eletronico: true },
+    { nome: "computador", preco: 4500.00, eletronico: true },
+    { nome: "teclado", preco: 100.00, eletronico: false },
+    { nome: "mouser", preco: 75.00, eletronico: true },
+    { nome: "Cadeira Gamer", preco: 1775.00, eletronico: false }
+]
